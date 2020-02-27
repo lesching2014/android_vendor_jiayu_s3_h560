550ca8bb37f2bceb7f7ae406efa06c72b71b0797
h0sch180 <hoschi@anukis.de>
2020-02-27 19:46:58 +0000

    lib fix

--- Android.mk
+++ Android.mk
@@ -11,4 +11,27 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libdpframework
+LOCAL_MODULE_OWNER := MediaTek
+LOCAL_SRC_FILES_64 := lib64/libdpframework.so
+LOCAL_SRC_FILES_32 := lib/libdpframework.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libaudiocustparam
+LOCAL_MODULE_OWNER := MediaTek
+LOCAL_SRC_FILES_64 := lib64/libaudiocustparam.so
+LOCAL_SRC_FILES_32 := lib/libaudiocustparam.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+
 endif
