6a708ff6d36a2e997b9b8fcc534abcfac00cea28
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-09-05 10:12:20 +0800

    Update s3_h560-vendor-blobs.mk

--- s3_h560-vendor-blobs.mk
+++ s3_h560-vendor-blobs.mk
@@ -1,15 +1,15 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/app,system/app)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/app,$(TARGET_COPY_OUT_SYSTEM)/app)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/lib,$(TARGET_COPY_OUT_SYSTEM)/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/lib64,system/lib64)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/etc,$(TARGET_COPY_OUT_SYSTEM)/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/usr,system/usr)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/usr,$(TARGET_COPY_OUT_SYSTEM)/usr)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/xbin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/xbin,$(TARGET_COPY_OUT_SYSTEM)/xbin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/bin,$(TARGET_COPY_OUT_SYSTEM)/bin)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/vendor,$(TARGET_COPY_OUT_VENDOR))
